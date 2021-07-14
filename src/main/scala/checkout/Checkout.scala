f36f38ef7538b7dfdf1dc059d91bf57282b7bd05
Simms <simms1993@hotmail.co.uk>
2021-07-15 00:28:55 +0100

    WIP

--- src/main/scala/checkout/Checkout.scala
+++ src/main/scala/checkout/Checkout.scala
@@ -4,9 +4,11 @@ import model.{Discount, ItemPricingRule}
 
 case class Checkout(pricingRules: Map[String, ItemPricingRule]) {
   var basket: Map[String, Int] = Map()
+  var  b2:Map[String, Int] =Map()
 
   def scanToAdd(item: String): Map[String, Int] = {
     basket += (item -> (basket.getOrElse(item, 0) + 1))
+    applesBananas
     basket
   }
 
@@ -14,20 +16,51 @@ case class Checkout(pricingRules: Map[String, ItemPricingRule]) {
   def total(): BigDecimal = {
     basket.toList.map { item =>
       val itemPricingRule = pricingRules.get(item._1)
+      println(pricingRules.get(item._1))
+
       calculateBasketItemPrice(itemPricingRule.get, item._2)
     }.sum
   }
 
+  def applesBananas()  = {
+
+    basket match {
+      case x if x.keySet("Apple") || x.keySet("Banana") => {
+        b2= basket.filter( x => x._1 == "Apple" || x._1 == "Banana")
+        b2.toList.map{ item =>
+          val singlePrice = pricingRules.get(item._1).get.unitPrice
+        println(  (singlePrice, item))
+
+        }
+      }
+      case _ => basket
+    }
+//    if(basket.keySet("Apple") || basket.keySet("Banana")){
+//     b2 = basket.filter( x => x._1 == "Apple" || x._1 == "Banana")
+////      println(     basket.filter( x => x._1 == "Apple" || x._1 == "Banana"))
+////      println(basket.filter( x => x._1 == "Apple" || x._1 == "Banana").min._2)
 //
-//  def checkItems(): Map[String,Int] = {
+////      b2.toList.map{ item =>
+////        val singlePrice = pricingRules.get(item._1)
+////        calculateBasketItemPrice(singlePrice.get,  item._2)
+////
+////      }.sum.bigDecimal
+//
+//    }
+   // basket.filter( x => x._1 == "Apple" || x._1 == "Banana")
+  }
+
+//  def checkItems(item1:String, item2:String):Map[String, Int] = {
 //  //  basket.keySet.contains("Apple")
-//    if(basket.contains("Apple") && basket.contains("Banana")){
-//      (basket.get("Apple"), basket.get("Banana")) match {
-//        case ("Apple","Banana") =>
-//        case ("Banana", "Apple") =>
-//
+//    if(basket.keySet.contains(item1) && basket.keySet.contains(item2)){
+//      (basket.get(item1), basket.get(item2)) match {
+//        case (Some(x), Some(y)) if x < y => basket += (item1 -> (x - 1)); basket
+//        case (Some(y), Some(x)) if y < x => basket += (item2 -> (y - 1)); basket
+//        case _ => basket
 //      }
 //    }
+//    println(basket)
+//  basket
 //  }
 
   private def calculateBasketItemPrice(itemPricingRule: ItemPricingRule, totalQuantity: Int): BigDecimal = {
--- src/main/scala/checkout/CheckoutSystem.scala
+++ src/main/scala/checkout/CheckoutSystem.scala
@@ -4,16 +4,22 @@ import model.{Discount, ItemPricingRule}
 
 object CheckoutSystem extends App {
 
-  val rules = Map(
-    "Apple" -> ItemPricingRule(unitPrice = BigDecimal("0.6"), discount = Some(Discount(minimumQuantity = 2, specialPrice = BigDecimal("0.6")))),
-    "Orange" -> ItemPricingRule(unitPrice = BigDecimal("0.25"), discount = Some(Discount(minimumQuantity = 3, specialPrice = BigDecimal("0.5")))),
-    "Banana" -> ItemPricingRule(unitPrice = BigDecimal("0.20"), discount = Some(Discount(minimumQuantity = 1, specialPrice = BigDecimal("0.20")))
+  val itemRules = Map(
+    "Apple" -> ItemPricingRule(unitPrice = BigDecimal("0.60"), discount = Some(Discount(minimumQuantity = 2, specialPrice = BigDecimal("0.60")))),
+    "Orange" -> ItemPricingRule(unitPrice = BigDecimal("0.25"), discount = Some(Discount(minimumQuantity = 3, specialPrice = BigDecimal("0.50")))),
+    "Banana" -> ItemPricingRule(unitPrice = BigDecimal("0.20"), discount = Some(Discount(minimumQuantity = 2, specialPrice = BigDecimal("0.20")))
 
     ))
 
-  val basket = Seq("Apple", "Orange", "Banana")
-  val checkout = Checkout(rules)
+  val basketRules = Map(
+
+  )
+
+  val basket = Seq("Apple", "Banana", "Orange")
+  val checkout = Checkout(itemRules)
 
   basket.foreach(checkout.scanToAdd)
+// basket.foreach(checkout.applesBananas)
+
   println(checkout.total())
 }
